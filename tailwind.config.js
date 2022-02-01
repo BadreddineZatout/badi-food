d1ea19cf1c1a303da9014e5e4965d19c67c3b6e6
BadreddineZatout <hb_zatout@esi.dz>
2022-02-01 11:21:48 +0100

    init v1.0

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#FF6363",
+                secondary: {
+                  100: "E2E2D5",
+                  200: "888883",
+                },
+              },
+              fontFamily: {
+                body: ["Oswald"],
+              },
+        },
+    },
+    plugins: [],
+};
