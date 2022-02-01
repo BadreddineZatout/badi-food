17b40f8c2b20061314a6fd12444025ba8aff4529
BadreddineZatout <hb_zatout@esi.dz>
2022-02-01 20:13:42 +0100

    v2.1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,7 +26,7 @@
                 </div>
                 <ul class="text-sm mt-6 hidden md:block" id="menu">
                     <li class="text-gray-700 font-bold py-1 border-r-4 border-primary">
-                        <a href="{{route('Home')}}" class="px-4">
+                        <a href="{{ route('Home') }}" class="px-4">
                             <span>Home</span>
                             <svg class="w-5 inline-block ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
@@ -83,7 +83,7 @@
                 <div id="cards" class="mt-8 grid lg:grid-cols-3 gap-10">
                     <!-- cards go here -->
                     @foreach ($recipes as $recipe)
-                        <div class="card">
+                        <div class="card modal-open cursor-pointer" onclick="toggleModal({{ $recipe }})">
                             <img class="w-full h-32 sm:h-48 object-cover" src="{{ Storage::url($recipe->image) }}">
                             <div class="m-4">
                                 <span class="font-bold">{{ $recipe->title }}</span>
@@ -101,6 +101,7 @@
                     @endforeach
                 </div>
 
+
                 {{-- <h4 class="font-bold mt-12 pb-2 border-b border-gray-200">Most Popular</h4>
 
                 <div class="mt-8">
@@ -114,6 +115,50 @@
                     Load more</div>
             </div>
         </main>
+    </div>
+    <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+        <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+
+        <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+
+            <div
+                class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                    viewBox="0 0 18 18">
+                    <path
+                        d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+                    </path>
+                </svg>
+                <span class="text-sm">(Esc)</span>
+            </div>
+
+            <!-- Add margin if you want to see some of the overlay behind the modal-->
+            <div class="modal-content py-4 text-left px-6">
+                <!--Title-->
+                <div class="flex justify-between items-center pb-3 border-b border-gray-300">
+                    <p class="text-2xl font-bold" id="modal-header"></p>
+                    <div class="modal-close cursor-pointer z-50">
+                        <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                            viewBox="0 0 18 18">
+                            <path
+                                d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+
+                <!--Body-->
+                <p id="modal-body" class="mt-5"></p>
+
+
+                <!--Footer-->
+                <div class="flex justify-end pt-2">
+                    <button
+                        class="modal-close px-4 bg-primary p-3 rounded-lg text-white hover:shadow-lg">Close</button>
+                </div>
+
+            </div>
+        </div>
     </div>
 
 </body>
@@ -167,7 +212,54 @@ crossorigin="anonymous"></script>
             card.append(img);
             card.append(titleDiv);
             card.append(badgeDiv);
+            card.click(function (e) { 
+                e.preventDefault();
+                toggleModal(recipe);
+            });
             $('#cards').append(card);
         }
+    }
+
+    /* var openmodal = document.querySelectorAll('.modal-open')
+    for (var i = 0; i < openmodal.length; i++) {
+        openmodal[i].addEventListener('click', function(event) {
+            event.preventDefault()
+            toggleModal()
+        })
+    } */
+
+    const overlay = document.querySelector('.modal-overlay')
+    overlay.addEventListener('click', toggleModal)
+
+    var closemodal = document.querySelectorAll('.modal-close')
+    for (var i = 0; i < closemodal.length; i++) {
+        closemodal[i].addEventListener('click', toggleModal)
+    }
+
+    document.onkeydown = function(evt) {
+        evt = evt || window.event
+        var isEscape = false
+        if ("key" in evt) {
+            isEscape = (evt.key === "Escape" || evt.key === "Esc")
+        } else {
+            isEscape = (evt.keyCode === 27)
+        }
+        if (isEscape && document.body.classList.contains('modal-active')) {
+            toggleModal()
+        }
+    };
+
+
+    function toggleModal(recipe) {
+        console.log(recipe);
+        const body = document.querySelector('body')
+        const modal = document.querySelector('.modal')
+        const modal_header = document.querySelector('#modal-header');
+        const modal_body = document.querySelector('#modal-body');
+        modal_header.innerText = recipe.title + ' - ' + recipe.author;
+        modal_body.innerText = recipe.instructions;
+        modal.classList.toggle('opacity-0')
+        modal.classList.toggle('pointer-events-none')
+        body.classList.toggle('modal-active')
     }
 </script>
